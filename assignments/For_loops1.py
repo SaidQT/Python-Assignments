14b3a154e1ad91ac769d250a692185bee4c1c378
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-04-28 15:10:58 +0300

    for loops 1 assignments addition

--- assignments/For_loops1.py
+++ assignments/For_loops1.py
@@ -1,9 +1,29 @@
-# Basic - Print all integers from 0 to 150.
-# Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-# Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
-# Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
-# Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
-# Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
-
 for i in range (151):
     print (i)
+    
+for multiples in range (0,1001,5):
+    print(multiples)
+    
+for int in range (1,101,1):
+    if int % 5==0:
+        print ("Coding")
+    elif int % 10 ==0:
+        print ("Coding Dojo")
+    else:
+        (print(int))
+
+sum = 0
+for odd in range(1, 500001, 2): 
+    sum += odd
+    print("The final sum:", sum)
+
+for positive in range (2018,0,-4):
+    print(positive)
+
+lowNum=2
+highNum=9
+mult=3
+
+for num in range (lowNum,highNum+1,1):
+    if num % mult==0:  
+        print (num)
