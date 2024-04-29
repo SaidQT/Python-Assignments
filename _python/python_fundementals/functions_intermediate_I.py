1b03ab85b908a641f50fb2bebd206b782580dec7
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-04-29 17:25:07 +0300

    adding function intermediate 1 assignment

---
+++ _python/python_fundementals/functions_intermediate_I.py
@@ -0,0 +1,15 @@
+import random
+def randInt(min= None, max=None):
+    num = random.random()
+    if min==None and max == None:
+        num=round(num*100)
+    elif min==None and max!=None:
+        num=round(num*50)
+    elif min!=None and max==None:
+        num=round(num*(100-min)+min)
+    elif min!=None and max!=None:
+        num=round(num*(max-min)+min)
+    return num
+
+
+
