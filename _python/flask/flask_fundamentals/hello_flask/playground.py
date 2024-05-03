19c7bef767a8c6c9b74b47a4ea0b255dce2ed2fa
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-05-04 00:48:39 +0300

    adding playground assignment

---
+++ _python/flask/flask_fundamentals/hello_flask/playground.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template # type: ignore
+app = Flask(__name__)    
+@app.route('/play')          
+def playGround():
+    return render_template('index.html',var=3 , color='#9fc5f8')
+@app.route('/play/<num>')
+def multipleBoxes(num):
+    return render_template('index.html',var=int(num),color='#9fc5f8')
+@app.route('/play/<num>/<color>')
+def multipleBoxesColor(num,color):
+    return render_template('index.html',var=int(num),color=color )
+if __name__=="__main__":     
+    app.run(debug=True)    
