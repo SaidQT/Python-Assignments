a32990d6b69dc278c14e3ea8edcf43e266075de3
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-05-14 16:05:48 +0300

    adding users with template assignment

--- _python/django/django_intro/ninja_gold/gold/views.py
+++ _python/django/django_intro/ninja_gold/gold/views.py
@@ -10,26 +10,19 @@ def play(request):
     if 'results' not in request.session:
         request.session['results']=[]
     if request.method=="POST":
-        if request.POST['game'] == 'cave':
+        if request.POST['game'] == 'cave' or request.POST['game']=='farm' or request.POST['game']=='house':
             request.session['random_num']=random.randint(10,20)
-            request.session['results'].append(f"You entered a cave and earned {request.session['random_num']}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
-        if request.POST['game'] == 'house':
-            request.session['random_num']=random.randint(10,20)
-            request.session['results'].append(f"You entered a house and earned {request.session['random_num']}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
-        if request.POST['game']== 'farm':
-            request.session['random_num']=random.randint(10,20)
-            request.session['results'].append(f"You entered a farm and earned {request.session['random_num']}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
-        if request.POST['game']=='quest':
+            request.session['results'].append(f"You entered a {request.POST['game']} and earned {request.session['random_num']}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
+        elif request.POST['game']=='quest':
             request.session['random_num']=random.randint(-50,50)
             if request.session['random_num'] >0:
                 request.session['results'].append(f"You completed a quest and earned {request.session['random_num']}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
-            if request.session['random_num'] < 0:
+            elif request.session['random_num'] < 0:
                 request.session['results'].append(f"You failed a quest and lost {abs(request.session['random_num'])}. {strftime("%Y-%m-%d %H:%M %p", localtime())}")
         request.session['gold']+=request.session['random_num']
     return redirect('/')
     
 def reset(request):
-    del request.session['gold']
-    del request.session['results']
+    request.session.clear()
     return redirect('/')
     
--- _python/django/django_intro/single_model_orm/users_app/views.py
+++ _python/django/django_intro/single_model_orm/users_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Users
+def index(request):
+    context={
+        "users":Users.objects.all()
+    }
+    return render(request,"index.html",context)
 
-# Create your views here.
+def user(request):
+    
+    first= request.POST['first_name']
+    last=request.POST['last_name']
+    email=request.POST['Email']
+    age=request.POST['age']
+
+    create= Users.objects.create(first_name=first,last_name=last,email_address=email,age=age)
+    return redirect('/')
+    
