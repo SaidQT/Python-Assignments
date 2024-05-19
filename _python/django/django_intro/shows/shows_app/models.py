285ab2a50c6d9e7f57dacc1c500d9c57dffce425
SaidQT <165608911+SaidQT@users.noreply.github.com>
2024-05-19 22:24:56 +0300

    validation assignment

--- _python/django/django_intro/shows/shows_app/models.py
+++ _python/django/django_intro/shows/shows_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Show name should be at least two characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show network should be at least three characters"
+        if len(postData['desc'])<10:
+            errors['desc']="Show description should be at least ten characters"
+        return errors
+    
 class Show(models.Model):
     title=models.CharField(max_length=45)
     network=models.CharField(max_length=45)
@@ -7,3 +19,4 @@ class Show(models.Model):
     desc=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    objects=ShowManager()
--- _python/django/django_intro/shows/shows_app/views.py
+++ _python/django/django_intro/shows/shows_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Show
+from django.contrib import messages
+
 def red(request):
     return redirect('/shows/')
 def index(request):
@@ -12,13 +14,20 @@ def new(request):
     return render(request,"new.html")
 
 def add(request):
-    if request.method=="POST":
-        title=request.POST['title']
-        network=request.POST['network']
-        release_date=request.POST['release_date']
-        desc=request.POST['desc']        
-    obj_x=Show.objects.create(title=title,network=network,release_date=release_date,desc=desc)
-    return redirect(f"shows/{obj_x.id}")
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('shows/new')
+    else:
+        if request.method=="POST":
+            title=request.POST['title']
+            network=request.POST['network']
+            release_date=request.POST['release_date']
+            desc=request.POST['desc']        
+        obj_x=Show.objects.create(title=title,network=network,release_date=release_date,desc=desc)
+        messages.success(request, "Show successfully added")
+        return redirect(f"shows/{obj_x.id}")
 
 def delete(request,id):
     show=Show.objects.get(id=id)
@@ -39,10 +48,17 @@ def edit(request,id):
     return render(request,'edit.html',data)
 
 def update(request,id):
-    edit_show=Show.objects.get(id=id)
-    edit_show.title=request.POST['title']
-    edit_show.network=request.POST['network']
-    edit_show.desc=request.POST['desc']
-    edit_show.release_date=request.POST['release_date']
-    edit_show.save()
-    return redirect('/')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        edit_show=Show.objects.get(id=id)
+        edit_show.title=request.POST['title']
+        edit_show.network=request.POST['network']
+        edit_show.desc=request.POST['desc']
+        edit_show.release_date=request.POST['release_date']
+        edit_show.save()
+        messages.success(request, "Show successfully updated")
+        return redirect('/')
